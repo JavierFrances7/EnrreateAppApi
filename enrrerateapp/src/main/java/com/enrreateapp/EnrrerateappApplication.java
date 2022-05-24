a2c34daae281b4f976f4e6f5fd59384f4029551e
JAVI-FRANCES\Javier <javierfrances7@gmail.com>
2022-05-24 20:30:55 +0200

    Creación Proyecto Api Spring Boot 2.7.0

---
+++ enrrerateapp/src/main/java/com/enrreateapp/EnrrerateappApplication.java
@@ -0,0 +1,13 @@
+package com.enrreateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrrerateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrrerateappApplication.class, args);
+	}
+
+}
---
+++ enrrerateapp/src/test/java/com/enrrerateapp/EnrrerateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrrerateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrrerateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
