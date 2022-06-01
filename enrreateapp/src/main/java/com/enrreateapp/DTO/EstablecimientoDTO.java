651db2a69428ade5f62dc413649407fbe03667c4
JAVI-FRANCES\Javier <javierfrances7@gmail.com>
2022-06-01 20:29:41 +0200

    Correcciones establecimiento DTO

--- enrreateapp/src/main/java/com/enrreateapp/DTO/EstablecimientoDTO.java
+++ enrreateapp/src/main/java/com/enrreateapp/DTO/EstablecimientoDTO.java
@@ -10,5 +10,11 @@ public class EstablecimientoDTO {
 	private String nombreEstablecimiento;
 	private String nombreGestor;
 	private String correo;
+	private float latitud;
+	private float longitud;
+	private String ciudad;
+	private String direccion;
+	private int aforoMaximo;
+	private float valoracionMedia;
 
 }
